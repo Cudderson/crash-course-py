f4ec47eede0d6fd34a6b1bbffbdc97c2772a280c
cudderson <codered1227@gmail.com>
2021-01-13 15:50:33 -0800

    module creation in pycharm

---
+++ chapter-9/chapter-9-mods/admin_mods.py
@@ -0,0 +1,50 @@
+# 9-11, mods for users_cc
+
+
+class User:
+    """basic blueprint for a person"""
+
+    def __init__(self, first_name, last_name, age, eye_color):
+        """Initialize attributes for a person"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.eye_color = eye_color
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f"This person's name is {self.first_name.title()} {self.last_name.title()}.")
+        print(f"{self.first_name.title()} is {self.age} years old.")
+        print(f"{self.first_name} has {self.eye_color} eyes!")
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name} {self.last_name}!")
+        print("\n")
+
+    def increment_login_attempts(self):
+        print(f"{self.first_name} {self.last_name} attempted to login.")
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        print(f"Resetting login attempts for {self.first_name}...\n")
+        self.login_attempts = 0
+
+
+class Admin(User):
+    """child class of 'User', specifically for admins"""
+
+    def __init__(self, first_name, last_name, age, eye_color):
+        super().__init__(first_name, last_name, age, eye_color)
+        # attribute as an instance of class Privileges
+        self.privileges = Privileges()
+
+
+class Privileges:
+    """Describes privileges of a user"""
+    def __init__(self):
+        self.privileges_list = ["can ban users", "can view logistics", "can push new code"]
+
+    def show_privileges(self):
+        print(f"\nThese are the special privilege of Admin:")
+        for privilege in self.privileges_list:
+            print(f"\t- {privilege}")
--- chapter-9/users_cc.py
+++ chapter-9/users_cc.py
@@ -1,59 +1,12 @@
 # 9-3, describing a person using Class
-# 9-7, creating subclass 'Admin' for class 'User'
+# 9-7 9-8, creating subclass 'Admin' for class 'User', and class 'Privileges'
 
-class User:
-    """basic blueprint for a person"""
-
-    def __init__(self, first_name, last_name, age, eye_color):
-        """Initialize attributes for a person"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.eye_color = eye_color
-        self.login_attempts = 0
-
-    def describe_user(self):
-        print(f"This person's name is {self.first_name.title()} {self.last_name.title()}.")
-        print(f"{self.first_name.title()} is {self.age} years old.")
-        print(f"{self.first_name} has {self.eye_color} eyes!")
-
-    def greet_user(self):
-        print(f"Hello, {self.first_name} {self.last_name}!")
-        print("\n")
-
-    def increment_login_attempts(self):
-        print(f"{self.first_name} {self.last_name} attempted to login.")
-        self.login_attempts += 1
-
-    def reset_login_attempts(self):
-        print(f"Resetting login attempts for {self.first_name}...\n")
-        self.login_attempts = 0
-
-
-class Admin(User):
-    """child class of 'User', specifically for admins"""
-
-    def __init__(self, first_name, last_name, age, eye_color):
-        super().__init__(first_name, last_name, age, eye_color)
-        # attribute as an instance of class Privileges
-        self.privileges = Privileges()
-
-
-class Privileges:
-    """Describes privileges of a user"""
-    def __init__(self):
-        self.privileges_list = ["can ban users", "can view logistics", "can push new code"]
-
-    def show_privileges(self):
-        print(f"\nThese are the special privilege of Admin:")
-        for privilege in self.privileges_list:
-            print(f"\t- {privilege}")
-
+import admin_mods as am
 
 # instances of User class
-user1 = User("John", "Williams", 29, "blue")
-user2 = User("Henry", "Bittersweet", 44, "brown")
-user3 = User("Cody", "Wells", 26, "red")
+user1 = am.User("John", "Williams", 29, "blue")
+user2 = am.User("Henry", "Bittersweet", 44, "brown")
+user3 = am.User("Cody", "Wells", 26, "red")
 
 # describe and greet each user
 user1.describe_user()
@@ -80,9 +33,13 @@ user3.reset_login_attempts()
 print(f"Login attempts for {user3.first_name} is now {user3.login_attempts}\n")
 
 # create instance of 'Admin' child class,
-main_admin = Admin("Viktor", "Krum", 35, "brown")
+main_admin = am.Admin("Viktor", "Krum", 35, "brown")
 main_admin.privileges.show_privileges()
 
 # appending string to instance attribute list 'privileges_list' in class 'Privileges' for 'main_admin'
 main_admin.privileges.privileges_list.append("be the boss")
 main_admin.privileges.show_privileges()
+
+new_admin = am.Admin("Cody", "Wells", 26, "brown")
+new_admin.privileges.show_privileges()
+new_admin.greet_user()
