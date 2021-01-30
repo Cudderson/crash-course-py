604ab772ff83aa666c7832d249908972cffba366
cudderson <codered1227@gmail.com>
2021-01-30 12:30:36 -0800

    create model topic, adjust gitignore

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """A topic the user is learning about"""
+
+    text = models.CharField(max_length=200)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):  # returns string representation of object
+        """Return a string representing the model"""
+
+        return self.text
+
