e555007be82493ba1c297d0731f18c975b6a659d
cudderson <codered1227@gmail.com>
2021-02-05 15:38:40 -0800

    added registration functionality, user-specifc data started

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,6 +11,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Show all topics"""
 
@@ -19,6 +21,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
 
@@ -28,6 +31,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Add a New Topic"""
 
@@ -46,6 +50,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry"""
 
@@ -68,6 +73,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
 
@@ -87,4 +93,3 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
-
--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Include django default auth urls
     path('', include('django.contrib.auth.urls')),
+
+    # Registration Page
+    path('register/', views.register, name='register')
 ]
